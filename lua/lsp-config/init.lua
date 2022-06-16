54f35f1b3aaad1ec4816a8a17d39bc87c53d16ac
Jorge Ramirez <jorge_ramirez@outlook.com>
2022-06-15 19:26:09 -0500

    add sumneko_lua vim support

--- lua/lsp-config/init.lua
+++ lua/lsp-config/init.lua
@@ -9,12 +9,6 @@ require("nvim-lsp-installer").setup({
     }
 })
 
-local lspconfig = require('lspconfig')
-
--- lspconfig.sumneko_lua.setup {}
--- lspconfig.pyright.setup{}
--- lspconfig.emmet_ls.setup{}
-
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
@@ -27,6 +21,26 @@ for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
     capabilities = capabilities,
+      settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'},
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
+      },
+    },
   }
 end
 
