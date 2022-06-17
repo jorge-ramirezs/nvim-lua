e3be36e5221fb285fcb3a93844e0beca541f535c
Jorge Ramirez <jorge_ramirez@outlook.com>
2022-06-17 13:11:28 -0500

    Add Comment.nvim plugin

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -1,63 +1,72 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+	packer_bootstrap = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
 end
 
-return require('packer').startup(function(use)
-  -- My plugins here
-  use {
-    'wbthomason/packer.nvim', -- Packer can manage itself
-    'kyazdani42/nvim-web-devicons',
-    'kyazdani42/nvim-tree.lua',
+return require("packer").startup(function(use)
+	-- My plugins here
+	use({
+		"wbthomason/packer.nvim", -- Packer can manage itself
+		"kyazdani42/nvim-web-devicons",
+		"kyazdani42/nvim-tree.lua",
 
-    -- Telescope
-    {
-      'nvim-telescope/telescope.nvim',
-      requires = { {'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter'} }
-    },
+		-- Telescope
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-telescope/telescope.nvim",
 
-    -- Fugitive
-    'tpope/vim-fugitive',
+		-- Fugitive
+		"tpope/vim-fugitive",
 
-    -- Is Like Gitlens
-    'f-person/git-blame.nvim',
+		-- Is Like Gitlens
+		"f-person/git-blame.nvim",
 
-    -- LSP Config, Installer and AutoCompletion
-    -- https://github.com/neovim/nvim-lspconfig/wiki/Installing-language-servers#automatically
-    -- https://github.com/neovim/nvim-lspconfig/wiki/Autocompletion
-    'williamboman/nvim-lsp-installer',
-    'neovim/nvim-lspconfig',
+		-- LSP Config, Installer and AutoCompletion
+		-- https://github.com/neovim/nvim-lspconfig/wiki/Installing-language-servers#automatically
+		-- https://github.com/neovim/nvim-lspconfig/wiki/Autocompletion
+		"williamboman/nvim-lsp-installer",
+		"neovim/nvim-lspconfig",
 
-    -- AutoCompletion
-    'hrsh7th/nvim-cmp', -- Autocompletion plugin
-    'hrsh7th/cmp-nvim-lsp', -- LSP source for nvim-cmp
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-nvim-lua',
-    'hrsh7th/cmp-buffer',
-    'saadparwaiz1/cmp_luasnip', -- Snippets source for nvim-cmp
-    'L3MON4D3/LuaSnip', -- Snippets plugin
-    'onsails/lspkind-nvim',
+		-- AutoCompletion
+		"hrsh7th/nvim-cmp", -- Autocompletion plugin
+		"hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-nvim-lua",
+		"hrsh7th/cmp-buffer",
+		"saadparwaiz1/cmp_luasnip", -- Snippets source for nvim-cmp
+		"L3MON4D3/LuaSnip", -- Snippets plugin
+		"onsails/lspkind-nvim",
 
-    -- AutoFormatting
-    'jose-elias-alvarez/null-ls.nvim',
+		-- AutoFormatting
+		"jose-elias-alvarez/null-ls.nvim",
 
-    -- bufferline, show buffer names in tabs
-    {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'},
+		-- NerdComment.nvim
+		"numToStr/Comment.nvim",
 
-    -- Pretty Status line
-    {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    },
+		-- bufferline, show buffer names in tabs
+		{ "akinsho/bufferline.nvim", tag = "v2.*", requires = "kyazdani42/nvim-web-devicons" },
 
-    -- Themes
-    "EdenEast/nightfox.nvim"
-  }
+		-- Pretty Status line
+		{
+			"nvim-lualine/lualine.nvim",
+			requires = { "kyazdani42/nvim-web-devicons", opt = true },
+		},
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+		-- Themes
+		"EdenEast/nightfox.nvim",
+	})
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
