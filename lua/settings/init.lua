dd5b0d2aa4747ceff6c1d5314cd7c57d809f2218
Jorge Ramirez <jorge_ramirez@outlook.com>
2022-07-02 15:07:31 -0500

    Update Setting

--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -4,11 +4,10 @@ set.termguicolors = true
 set.cursorline = true
 set.laststatus = 3
 
-
 set.hidden = true
 -- set.path += '**'
 set.wrap = false
-set.encoding = 'UTF-8'
+set.encoding = "UTF-8"
 
 set.number = true
 set.relativenumber = true
@@ -25,18 +24,18 @@ set.smartcase = true
 set.scrolloff = 8
 set.incsearch = true
 set.hlsearch = false
-set.signcolumn = 'yes'
-set.colorcolumn = '80'
-vim.highlight.create('ColorColumn', {ctermbg=0, guibg='lightgrey'}, false)
+set.signcolumn = "yes"
+set.colorcolumn = "80"
+vim.highlight.create("ColorColumn", { ctermbg = 0, guibg = "lightgrey" }, false)
 
 set.swapfile = false
 set.backup = false
 set.undofile = true
-set.undodir = vim.fn.stdpath('data') .. '/undodir'
+set.undodir = vim.fn.stdpath("data") .. "/undodir"
 -- execute 'set undodir=' . g:nvim_data_root . '/undodir'
 --
 -- Python virtual environment detection in linux
 -- -- let g:loaded_python_provider = 0
 -- -- let g:loaded_python3_provider = 0
 -- let g:python3_host_prog = '/usr/bin/python3'
-vim.g.python3_host_prog = '/home/jorge/.local/share/virtualenvs/nvim-86YW0UDp/bin/python'
+vim.g.python3_host_prog = "/home/jorge/.local/share/virtualenvs/nvim-86YW0UDp/bin/python"
