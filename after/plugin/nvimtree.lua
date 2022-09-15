6b7ad0a256b54b656af1f0d5a6e5253bb1ff74a2
Jorge Ramirez <jorge_ramirez@outlook.com>
2022-09-15 13:32:29 -0500

    setup NvimTree

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -1,4 +1,16 @@
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
+
 require("nvim-tree").setup({
+	git = {
+		ignore = false,
+	},
+	actions = {
+		open_file = {
+			quit_on_open = true,
+		},
+	},
 	diagnostics = {
 		enable = true,
 	},
