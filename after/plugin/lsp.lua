d195dc34a3f57a2793ff417e3585b5ce0aa2f03f
Jorge Ramirez <jorge_ramirez@outlook.com>
2022-10-15 19:36:14 -0500

    replace update_capabilities

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,6 +1,7 @@
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+-- capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 require("nvim-lsp-installer").setup({
 	automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
